b3f8666e68446223ffe0f6c71820e887132d49b3
Roel Noback <roel@noback.com>
2016-03-09 17:31:29 +0100

    setup comments page

--- src/js/controllers/comment.js
+++ src/js/controllers/comment.js
@@ -3,9 +3,9 @@
     'use strict';
     var converterApp = angular.module('converterApp');
     
-    converterApp.controller('timeController', 
-        ['$scope', '$filter', 'unitSelectionService', 'formatNumberFactory',
-        function($scope, $filter, unitSelectionService, formatNumberFactory) {
+    converterApp.controller('commentController', 
+        ['$scope', '$filter', '$location', 'unitSelectionService', 'formatNumberFactory',
+        function($scope, $filter, $location, unitSelectionService, formatNumberFactory) {
 
     $scope.unitsCollection = unitSelectionService.units;
     $scope.defaultButtonText = unitSelectionService.defaultButtonText; 
@@ -19,190 +19,124 @@
             
         
     // Get last segment of URL
-    var url = document.URL;
-    $scope.unit = url.substr(url.lastIndexOf('/') + 1); 
+    var url = $location.path();
+    $scope.selectedUnit = url.substr(url.lastIndexOf('/') + 1); 
+
     // Add it to text button    
-    $scope.unitCustomTexts = {buttonDefaultText:$scope.unit};
+    $scope.unitCustomTexts = {buttonDefaultText:$scope.selectedUnit};
     
     $scope.redirectUnit = unitSelectionService.redirectUnit;
     
     $scope.unit1 = {
         id: 1, 
-        label: 'Seconds', 
-        maker: 's', 
-        cname: 'unit1', 
+        label: 'Watt Hour', 
+        marker: '', 
+        cname: 'unit1',
         ticked: true,
         value: 0,
         factor: 1,
         type: ''
-    };    
+    };
+
     $scope.unit2 = {
         id: 2, 
-        label: 'Minutes', 
-        maker: 'm', 
-        cname: 'unit2',
-        ticked: true,
+        label: 'Erg',
+        maker: 'kW', 
+        cname: 'unit2', 
+        ticked: false,
         value: 0,
-        factor:0.0166666666666667,
+        factor: 36000000000,
         type: ''
-    };
+    };    
+
     $scope.unit3 = {
         id: 3, 
-        label: 'Hours', 
-        marker: 'h', 
+        label: 'Joule (watt second)', 
+        maker: '', 
         cname: 'unit3',
         ticked: true,
         value: 0,
-        factor: 2.777777777777778e-4,
+        factor: 3600,
         type: ''
     };
+   
     $scope.unit4 = {
         id: 4, 
-        label: 'Days', 
+        label: 'Kilowatt Hour (KWH)', 
         marker: '', 
         cname: 'unit4',
         ticked: true,
         value: 0,
-        factor: 1.15741E-05,
+        factor: 0.001,
         type: ''
     };
     $scope.unit5 = {
         id: 5, 
-        label: 'Weeks', 
+        label: 'BTU', 
         marker: '', 
         cname: 'unit5', 
         ticked: false,
         value: 0,
-        factor: 1.65344E-06,
+        factor: 3.412141633,
         type: ''
     };
+
     $scope.unit6 = {
         id: 6, 
-        label: 'Fortnight', 
-        maker: '', 
+        label: 'Therm (US)', 
+        marker: '', 
         cname: 'unit6', 
         ticked: false,
         value: 0,
-        factor: 8.2672E-07,
+        factor: 3.41296E-05,
         type: ''
     };
     $scope.unit7 = {
         id: 7, 
-        label: 'Months (1/12th yr)', 
-        maker: '(365 day, except for last 2)', 
+        label: 'Foot-pounds ', 
+        marker: '', 
         cname: 'unit7', 
         ticked: false,
         value: 0,
-        factor: 3.80518E-07,
+        factor: 2655.223737,
         type: ''
     };
     $scope.unit8 = {
         id: 8, 
-        label: 'Year (365 day)', 
-        maker: '', 
+        label: 'Calorie (heat)', 
+        marker: '', 
         cname: 'unit8', 
         ticked: false,
         value: 0,
-        factor: 3.17098E-08,
+        factor: 859.8452279,
         type: ''
     };
     $scope.unit9 = {
         id: 9, 
-        label: 'Year (leap)', 
-        maker: '', 
+        label: 'Calorie (food)', 
+        marker: '', 
         cname: 'unit9', 
-        ticked: false,
-        value: 0,
-        factor: 3.16232E-08,
-        type: ''
-    };
-    $scope.unit10 = {
-        id: 10, 
-        label: 'Year (mean)', 
-        maker: '', 
-        cname: 'unit10', 
-        ticked: false,
-        value: 0,
-        factor: 3.16888E-08,
-        type: ''
-    };
-    $scope.unit11 = {
-        id: 11, 
-        label: 'Picosecond',
-        maker: 'ps',
-        cname: 'unit11',
-        ticked: false,
-        value: 0,
-        factor: 1E+12,
-        type: ''
-    };
-    $scope.unit12 = {
-        id: 12, 
-        label: 'Nanosecond',
-        maker: 'ns',
-        cname: 'unit12',
-        ticked: false,
-        value: 0,
-        factor: 1000000000,
-        type: ''
-    };
-    $scope.unit13 = {
-        id: 13, 
-        label: 'Microsecond',
-        maker: 'us',
-        cname: 'unit13',
-        ticked: false,
-        value: 0,
-        factor: 1000000,
-        type: ''
-    };
-    $scope.unit14 = {
-        id: 14, 
-        label: 'Millisecond', 
-        maker: '',
-        cname: 'unit14',
-        ticked: false,
-        value: 0,
-        factor: 1000,
-        type: ''
-    };
-
-    $scope.unit15 = {
-        id: 15, 
-        label: 'Hr/Min/Sec', 
-        maker: '',
-        cname: 'unit15',
         ticked: true,
         value: 0,
-        value1: 0,
-        value2: 0,
-        factor: 2.777777777777778e-4,
-        type: '',
-        special: 'triple',
+        factor: 0.859845228,
+        type: ''
     };
+   
     
     $scope.dataCollection = 
     [
-        $scope.unit11,
-        $scope.unit12,
-        $scope.unit13,
-        $scope.unit14,
-        $scope.unit1, 
         $scope.unit2, 
         $scope.unit3, 
-        $scope.unit15,
-        $scope.unit4,
-        $scope.unit5, 
-        $scope.unit6,
         $scope.unit7,
         $scope.unit8,
+        $scope.unit5,
+        $scope.unit1,
         $scope.unit9,
-        $scope.unit10
+        $scope.unit4,
+        $scope.unit6        
     ];
     
     
-
-
 
     // collapse window => needs to go in a directive
     $scope.windowOpen = true;
@@ -226,12 +160,6 @@
             $scope.windowOpen = true;
         }
     });
-
-
-
-
-
-
 
 
     
@@ -282,7 +210,7 @@
         curUnit.ticked = true;
         $scope.unitsTicked.push(curUnit);
     };
-    
+     
     $scope.removeUnit = function(curUnit){ 
         for(var i=0; i<$scope.unitsTicked.length; i++){
             if($scope.unitsTicked[i].id === curUnit.id){
@@ -330,58 +258,30 @@
     $scope.$watch('unit9.value', function(){
         $scope.unit1.value = formatNumberFactory.formatNumber($scope.unit9.value / $scope.unit9.factor);
     });
-    $scope.$watch('unit10.value', function(){
-        $scope.unit1.value = formatNumberFactory.formatNumber($scope.unit10.value / $scope.unit10.factor);
-    });
-    $scope.$watch('unit11.value', function(){
-        $scope.unit1.value = formatNumberFactory.formatNumber($scope.unit11.value / $scope.unit11.factor);
-    });
-    $scope.$watch('unit12.value', function(){
-        $scope.unit1.value = formatNumberFactory.formatNumber($scope.unit12.value / $scope.unit12.factor);
-    });
-    $scope.$watch('unit13.value', function(){
-        $scope.unit1.value = formatNumberFactory.formatNumber($scope.unit13.value / $scope.unit13.factor);
-    });
-    $scope.$watch('unit14.value', function(){
-        $scope.unit1.value = formatNumberFactory.formatNumber($scope.unit14.value / $scope.unit14.factor);
-    });
+   
     
-    
-     // MAIN unit
+     // Main unit
     $scope.$watch('unit1.value', function(){
         $scope.unit2.value = formatNumberFactory.formatNumber($scope.unit1.value * $scope.unit2.factor);
         $scope.unit3.value = formatNumberFactory.formatNumber($scope.unit1.value * $scope.unit3.factor);
         $scope.unit4.value = formatNumberFactory.formatNumber($scope.unit1.value * $scope.unit4.factor);
-        $scope.unit5.value = formatNumberFactory.formatNumber($scope.unit1.value * $scope.unit5.factor);
-        $scope.unit6.value = formatNumberFactory.formatNumber($scope.unit1.value * $scope.unit6.factor);
+        $scope.unit5.value = formatNumberFactory.formatNumber($scope.unit1.value * $scope.unit5.factor); 
+        $scope.unit6.value = formatNumberFactory.formatNumber($scope.unit1.value * $scope.unit6.factor); 
         $scope.unit7.value = formatNumberFactory.formatNumber($scope.unit1.value * $scope.unit7.factor);
-        $scope.unit8.value = formatNumberFactory.formatNumber($scope.unit1.value * $scope.unit8.factor);
-        $scope.unit9.value = formatNumberFactory.formatNumber($scope.unit1.value * $scope.unit9.factor);
-        $scope.unit10.value = formatNumberFactory.formatNumber($scope.unit1.value * $scope.unit10.factor);
-        $scope.unit11.value = formatNumberFactory.formatNumber($scope.unit1.value * $scope.unit11.factor);
-        $scope.unit12.value = formatNumberFactory.formatNumber($scope.unit1.value * $scope.unit12.factor);
-        $scope.unit13.value = formatNumberFactory.formatNumber($scope.unit1.value * $scope.unit13.factor);
-        $scope.unit14.value = formatNumberFactory.formatNumber($scope.unit1.value * $scope.unit14.factor);
+        $scope.unit8.value = formatNumberFactory.formatNumber($scope.unit1.value * $scope.unit8.factor); 
+        $scope.unit9.value = formatNumberFactory.formatNumber($scope.unit1.value * $scope.unit9.factor); 
     });
     */
+    formatNumberFactory.precision = 10;
+    $scope.calcHandler = function(obj, inputValue){
 
-    formatNumberFactory.precision = 5;
-    $scope.calcHandler = function(obj, inputValue, inputValue1, inputValue2){
-
-        var baseValue;
-        if(obj.special){
-            var minToHour = formatNumberFactory.formatNumber(inputValue1/60);
-            var secToHour = formatNumberFactory.formatNumber(inputValue2/3600);
-            var totalFeet = formatNumberFactory.formatNumber (parseInt(inputValue) + minToHour + secToHour);
-            baseValue = $scope.convertToBaseUnit(totalFeet, obj.factor);
-        }else{
-            baseValue = $scope.convertToBaseUnit(inputValue, obj.factor);
-        }
-
+        var baseValue = $scope.convertToBaseUnit(inputValue, obj.factor);
         $scope.convertUnitsFromBase( baseValue );
     }
 
     $scope.convertToBaseUnit = function(value, factor){
+        (math.bignumber($scope.unit5.value) / math.bignumber($scope.unit5.factor));
+
         return value/factor;
     }
 
@@ -395,30 +295,8 @@
         $scope.unit7.value = formatNumberFactory.formatNumber(baseUnitValue * $scope.unit7.factor);
         $scope.unit8.value = formatNumberFactory.formatNumber(baseUnitValue * $scope.unit8.factor);
         $scope.unit9.value = formatNumberFactory.formatNumber(baseUnitValue * $scope.unit9.factor);
-        $scope.unit10.value = formatNumberFactory.formatNumber(baseUnitValue * $scope.unit10.factor);
-        $scope.unit11.value = formatNumberFactory.formatNumber(baseUnitValue * $scope.unit11.factor);
-        $scope.unit12.value = formatNumberFactory.formatNumber(baseUnitValue * $scope.unit12.factor);
-        $scope.unit13.value = formatNumberFactory.formatNumber(baseUnitValue * $scope.unit13.factor);
-        $scope.unit14.value = formatNumberFactory.formatNumber(baseUnitValue * $scope.unit14.factor);
-
-        // to hours
-        var toHours = formatNumberFactory.formatNumber(baseUnitValue * $scope.unit15.factor);
-
-        console.log('toHours ' + toHours);
-        var restHours =  toHours % 1;
-        console.log('restHours ' + restHours);
-        $scope.unit15.value = formatNumberFactory.formatNumber(Math.floor(toHours));
-        
-        var toMinutes = parseFloat(restHours * 60);
-        $scope.unit15.value1 = formatNumberFactory.formatNumber(Math.floor(toMinutes));
-        
-        var restMinutes =  toMinutes % 1;
-        console.log('restMinutes ' + restMinutes);
-
-        var toSeconds = restMinutes * 60;
-        $scope.unit15.value2 = formatNumberFactory.formatNumber(toSeconds);
-        
     }
+
 }]);
     
     
